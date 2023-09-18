2efd9e904836c20385387ac1ada8b7005b9a937e
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-09-18 10:35:26 +0500

    faq accordion done

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,2 +1,3 @@
 import "./components/phone-input";
 import "./components/running-line";
+import "./components/accordion";
--- src/js/components/running-line.js
+++ src/js/components/running-line.js
@@ -2,12 +2,14 @@ import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".line", {
-	scrollTrigger: {
-		trigger: ".line",
-		start: "top 98%",
-		end: "bottom top",
-		scrub: true,
-	},
-	x: "-50%",
-});
+if (document.querySelector(".line")) {
+	gsap.to(".line", {
+		scrollTrigger: {
+			trigger: ".line",
+			start: "top 98%",
+			end: "bottom top",
+			scrub: true,
+		},
+		x: "-100%",
+	});
+}
